8df0cb3932c299a791e889bfeb0415bba7b2e97e
Alexandr Generalov <generalov@prioris.ru>
2014-12-08 17:30:51 +0300

    fix mogrations

--- db/migrate/004_change_wrong_default_values.rb
+++ db/migrate/004_change_wrong_default_values.rb
@@ -1,6 +1,6 @@
 class ChangeWrongDefaultValues < ActiveRecord::Migration
   def change
-    change_column_default :issues_import_emails, :category_id, 0
+    change_column_default :issues_import_emails, :issue_category_id, 0
     change_column_default :issues_import_emails, :issues_import_server_id, nil
   end
 end
